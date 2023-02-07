528063b6bf7722aa164faf26893767e96f3e4a80
thomashilger67 <hilger.thomas67@gmail.com>
2023-02-07 21:55:00 +0100

    ajout front

--- application/webservice/db/database.py
+++ application/webservice/db/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 
 def get_database():
-   uri ='mongodb://db_mongo_bis:27017'
+   uri ='mongodb://localhost:27017'
    client = MongoClient(uri)
 
    return client['activities']
